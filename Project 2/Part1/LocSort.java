297f497ba4c50f08286817cc362aee4c9c338c94
Derek H Tominaga <61033666+wakawaka121@users.noreply.github.com>
2023-02-06 15:42:21 -0700

    Add files via upload

---
+++ Project 2/Part1/LocSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class LocSort {
+    
+    /***
+     *locality-aware selection sort on array a
+     *with locality-aware value d
+     ***/
+    public static void selectionSort(int[] a, int d) {
+	//TO BE IMPLEMENTED
+    }
+
+    /***
+     *locality-aware insertion sort on array a
+     *with locality-aware value d
+     ***/
+    public static void insertionSort(int[] a, int d) {
+	//TO BE IMPLEMENTED
+    }
+
+    /***
+     *locality-aware heapsort on array a
+     *with locality-aware value d
+     ***/
+    public static void heapSort(int[] a, int d) {
+	//TO BE IMPLEMENTED
+    }
+
+    /***
+     *locality-aware mergeSort on array a 
+     *with locality-aware value d
+     ***/
+    public static void mergeSort(int[] a, int d) {
+	//TO BE IMPLEMENTED
+    }
+}
